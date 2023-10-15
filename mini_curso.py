c8946fd283e1d1558fcc77887808bbb1e665f424
Jefferson <40587602+jefftdb@users.noreply.github.com>
2023-10-15 14:00:54 -0300

    Melhorando a parte de batalha

--- mini_curso.py
+++ mini_curso.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 import time
 import colorama
 from colorama import Fore
@@ -42,37 +42,36 @@ class Heroi(Item):
             self.estaVivo = False
 
 
-player1 = Heroi('Jogador 1')
-player2 = Heroi('Jogador 2')
-player3 = Heroi('Jogador 3')
-
-jogadores = player1,player2,player3
 
 
-while((player1.estaVivo and player2.estaVivo) or (player1.estaVivo and player3.estaVivo) or (player2.estaVivo and player3.estaVivo)):
-    atacante = randint(1,3)
+jogadores = []
 
-    if atacante == 1 and player1.estaVivo:
-        vitima = randint(0,1)
-        if vitima == 0 and player2.estaVivo:          
-            player1.ataca(player3)
-        else:
-            player1.ataca(player2)
+while True:
+    player = input(Fore.GREEN + 'Digite o nome do Herói ou 0 para sair: ')
+    if player != '0':
 
-    elif atacante == 2 and player2.estaVivo :
-        vitima = randint(0,1)
-        if vitima == 0 and player1.estaVivo:          
-            player2.ataca(player1)
-        else:
-            player2.ataca(player3)
+        jogadores.append(Heroi(player))
+    else:
+        break
+    
 
-    elif atacante == 3 and player3.estaVivo:
-        vitima = randint(0,1)
-        if vitima == 0 and player1.estaVivo:          
-            player3.ataca(player1)
-        else:
-            player3.ataca(player2)
 
+
+
+while(len(jogadores) >=2):
+    atacante = random.choice(jogadores)
+    inimigo = random.choice(jogadores)
+
+    if atacante != inimigo:
+        atacante.ataca(inimigo)
+    else:
+        while atacante == inimigo:
+            inimigo = random.choice(jogadores)
+
+    
+    for jogador in jogadores: # remove da lista o Jogador Eliminado
+        if not jogador.estaVivo:
+            jogadores.remove(jogador)
 
 
 for jogador in jogadores: #Exibe o jogador vencedor
